ff95274512101bdc8840d9b98470f42dc42486e6
charlesavery8 <120284357+charlesavery8@users.noreply.github.com>
2022-12-22 14:49:11 -0800

    pcost.py solved
    **** with open('C:/Users/avery/OneDrive/Documents/GitHub/practical-python$$$/Work/Data/portfolio.csv', 'rt') as f: **** had to specify exact file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+Total_cost = 0
+with open('C:/Users/avery/OneDrive/Documents/GitHub/practical-python$$$/Work/Data/portfolio.csv', 'rt') as f:
+    skip = next(f)
+    for line in f:
+        row =line.split(",")
+        shares = float(row[1])
+        price = float(row[2])
+        Total_cost = price *shares
+print('Total Cost:', Total_cost)
