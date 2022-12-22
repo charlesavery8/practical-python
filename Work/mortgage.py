9ddd3a6ccd1d5ead47eae19a7bfff208103a4502
charlesavery8 <120284357+charlesavery8@users.noreply.github.com>
2022-12-22 13:11:37 -0800

    mortgage.py solved

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+extra = 1000
+st_extra = 60
+end_extra = 108
+
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    if st_extra < months <= end_extra :
+        principal -= extra
+        total_paid += extra
+    months += 1
+    if principal < 0:
+        total_paid -= abs(principal)
+        principal += abs(principal)    
+    print(months, total_paid, principal)
+
+print("Total Paid:", total_paid, "\n", "Months:", months)
+
